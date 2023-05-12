533f4d6be54a977aa096ba24c5a3a2128adf0bfe
E46\User <miguelcarlbasilio19@gmail.com>
2023-05-12 12:22:16 +0800

    second commit

--- PDC03_MODULE04/PDC03_MODULE04/MainPage.xaml.cs
+++ PDC03_MODULE04/PDC03_MODULE04/MainPage.xaml.cs
@@ -15,9 +15,25 @@ namespace PDC03_MODULE04
             InitializeComponent();
         }
 
-        private async void OpenActivity01(object sende, EventArgs e)
+        private async void OpenActivity01(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new activity01());
+        }
+        private async void OpenActivity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity02());
+        }
+        private async void OpenActivity03(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity03());
+        }
+        private async void OpenActivity04(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity04());
+        }
+        private async void OpenActivity05(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity05());
         }
     }
 }
---
+++ PDC03_MODULE04/PDC03_MODULE04/activity05.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity05 : ContentPage
+    {
+        List<Contacts> contacts;
+        public activity05()
+        {
+            InitializeComponent();
+
+            SetupData();
+            listView.ItemsSource = contacts;
+        
+        }
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(listView.SelectedItem != null) 
+            {
+                var detailPage = new DetailPage();
+                detailPage.BindingContext = listView.SelectedItem as Contacts;
+                listView.SelectedItem = null;
+                await Navigation.PushAsync(detailPage);
+            }
+        }
+
+        void SetupData()
+        {
+            contacts = new List<Contacts>();
+
+            contacts.Add(new Contacts
+            {
+                Name = "Siboo Yas",
+                Age = 25,
+                Occupation = "Tester",
+                Country = "Philippines"
+            });
+            contacts.Add(new Contacts
+            {
+                Name = "Khang Kong",
+                Age = 31,
+                Occupation = "Developer",
+                Country = "Taiwan"
+            });
+            contacts.Add(new Contacts
+            {
+                Name = "Lue Ya",
+                Age = 28,
+                Occupation = "Engineer",
+                Country = "Singapore"
+            });
+            contacts.Add(new Contacts
+            {
+                Name = "Patah Tas",
+                Age = 22,
+                Occupation = "Architect",
+                Country = "Hong Kong"
+            });
+            contacts.Add(new Contacts
+            {
+                Name = "Bah Wang",
+                Age = 35,
+                Occupation = "Doctor",
+                Country = "Japan"
+            });
+
+        }
+
+    }
+}
